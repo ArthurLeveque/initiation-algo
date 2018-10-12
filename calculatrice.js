c08e20a19d8e93c635e1363bdcef20f587edbec3
ArthurLeveque <arthur.lev44@gmail.com>
2018-10-12 14:42:57 +0200

    exo terminé

---
+++ calculatrice.js
@@ -0,0 +1,21 @@
+console.log('coucou');
+
+var c=0, a=2, b=3;
+c = a + b;
+
+console.log(a + ' + ' + b + ' = ' + c);
+
+c = a - b;
+
+console.log(a + ' - ' + b + ' = ' + c);
+
+c = a / b;
+
+console.log(a + ' / ' + b + ' = ' + c);
+
+c = a * b;
+
+console.log(a + ' * ' + b + ' = ' + c);
+
+
+
